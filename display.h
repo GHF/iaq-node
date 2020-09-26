ab60d9f9a15cbae88056fba30832320d76472169
Xo Wang <xo@geekshavefeelings.com>
2020-09-26 13:06:22 -0700

    ✨ Draw sensor readings to TFT display
    Add header file with drawing and text processing helpers and definitions
    of pages of text to be printed to the TFT LCD on the TTGO T-Display
    board.

---
+++ display.h
@@ -0,0 +1,114 @@
+// (C) Copyright 2020 Xo Wang <xo@geekshavefeelings.com>
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "esphome.h"
+
+#include <cstdio>
+#include <ctime>
+
+// This file is included only by the main translation unit
+namespace {
+
+using namespace esphome::display;
+using namespace esphome::sensor;
+
+struct Point {
+  int x;
+  int y;
+};
+
+struct Extents {
+  Point p;
+  int width;
+  int height;
+};
+
+Extents GetTextExtents(DisplayBuffer& it,
+                       Point start,
+                       Font* font,
+                       const char* text,
+                       TextAlign align = TextAlign::TOP_LEFT) {
+  Extents extents;
+  it.get_text_bounds(start.x, start.y, text, font, align, &extents.p.x,
+                     &extents.p.y, &extents.width, &extents.height);
+  return extents;
+}
+
+[[gnu::format(printf, 1, 2)]] std::string StringFormat(const char* fmt, ...) {
+  va_list args_measure;
+  va_start(args_measure, fmt);
+  va_list args_write;
+  va_copy(args_write, args_measure);
+  std::vector<char> output(std::vsnprintf(nullptr, 0, fmt, args_measure) + 1);
+  va_end(args_measure);
+  std::vsnprintf(output.data(), output.size(), fmt, args_write);
+  va_end(args_write);
+  return std::string(output.cbegin(), std::prev(output.cend()));
+}
+
+// Returns a point that moves down by the height of the text printed. Note that
+// this only makes sense for top-aligned text.
+Point DrawText(DisplayBuffer& it,
+               Point start,
+               Font* font,
+               const std::string& text,
+               TextAlign align = TextAlign::TOP_LEFT) {
+  it.print(start.x, start.y, font, align, text.c_str());
+  const auto extents = GetTextExtents(it, start, font, text.c_str(), align);
+  return Point{start.x, extents.p.y + extents.height};
+}
+
+enum class FontSize {
+  kTiny,
+  kSmall,
+  kNormal,
+  kLarge,
+  kHuge,
+};
+
+Font* GetFont(FontSize size) {
+  static Font* const kFonts[] = {&id(font_sans_12), &id(font_sans_17),
+                                 &id(font_sans_24), &id(font_sans_34),
+                                 &id(font_sans_48)};
+  return kFonts[static_cast<size_t>(size)];
+}
+
+struct Card {
+  std::string name;
+  Sensor* sensor;
+};
+
+struct Page {
+  std::array<Card, 2> cards;
+};
+
+void DrawPage(DisplayBuffer& it, size_t page_index) {
+  // Must be function statically initialized because id() can't be called at
+  // namespace init time.
+  static std::array kPages{
+      Page{{Card{.name = "Temperature", .sensor = &id(iaq_temperature)},
+            Card{.name = "Humidity", .sensor = &id(iaq_humidity)}}}};
+
+  Point cursor{(it.get_width() - 1) / 2, 0};
+  // Draw sensor reading cards from top to bottom
+  for (auto& [name, sensor] : kPages[page_index].cards) {
+    cursor = DrawText(it, cursor, GetFont(FontSize::kSmall), name,
+                      TextAlign::TOP_CENTER);
+    const auto value_str =
+        StringFormat("%.*f", sensor->get_accuracy_decimals(), sensor->state);
+    cursor = DrawText(it, cursor, GetFont(FontSize::kHuge), value_str,
+                      TextAlign::TOP_CENTER);
+    cursor = DrawText(it, cursor, GetFont(FontSize::kNormal),
+                      sensor->get_unit_of_measurement(), TextAlign::TOP_CENTER);
+  }
+
+  // Draw date and time at bottom
+  Point bottom_center{cursor.x, it.get_height() - 1};
+  cursor = DrawText(it, bottom_center, GetFont(FontSize::kSmall),
+                    id(time_esp).now().strftime("%b %d %H:%M"),
+                    TextAlign::BOTTOM_CENTER);
+}
+
+}  // namespace
